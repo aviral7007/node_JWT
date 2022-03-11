8e2b9819ab7b6d7d142e2eb6e24233e9ead50172
Aviral Yadav <aviral.yadav7007@gmail.com>
2022-03-11 15:57:45 +0530

    first-commit

---
+++ src/server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+  {
+    username: 'Aviral ',
+    title: 'trainee-1'
+  },
+  {
+    username: 'Himanshu',
+    title: 'trainee-2'
+  },
+  {
+    username: 'Aman',
+    title: 'trainee-3'
+  }
+]
+
+app.get('/getdata', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
+})
+app.post('/login',(req,res)=>{
+    const username=req.body.username
+    const user={name:username}
+
+    const accessToken=jwt.sign(user,`${process.env.ACCESS_TOKEN_SECRET}`)
+    res.json({accessToken:accessToken})
+})
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token,`${process.env.ACCESS_TOKEN_SECRET}`, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Server is up and running on ${PORT} ...`);
+});
